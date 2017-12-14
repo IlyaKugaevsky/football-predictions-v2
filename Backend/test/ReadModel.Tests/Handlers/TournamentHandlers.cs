e751668d44033966e0a17ccb083d743f575023a8
IlyaNotebook <ilya.kugaevsky@gmail.com>
2017-12-14 18:55:50 -0500

    Write first dummy test

---
+++ Backend/test/ReadModel.Tests/Handlers/TournamentHandlers.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Predictions.Persistence;
+
+namespace ReadModel.Tests.Handlers
+{
+    public class TournamentHandlers
+    {
+        [Fact]
+        public void Test1()
+        {
+            // var context = new PredictionsContext(); 
+            Assert.Equal(2, 2);
+        }
+    }
+}

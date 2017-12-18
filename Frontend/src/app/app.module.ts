246bd3f4121a59bfbf912764ecded61ef17cd062
IlyaNotebook <ilya.kugaevsky@gmail.com>
2017-12-18 06:02:38 -0500

    Add simple nav-bar

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from "./app-routing.module";
 import { TournamentsModule } from "./tournaments/tournaments.module";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./home/home-page.component";
+import { HeaderComponent } from "./shared/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
